065349b5f9fd99d4c7480bed773726da4fc7c034
shahparth36 <parthshah1936@gmail.com>
2021-07-09 19:32:44 +0530

    Added redirecting / to login page

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,14 @@
+import { Navigate } from 'react-router-dom';
+
 import LoginForm from './LoginForm';
 import RegisterForm from './RegisterForm';
 import Home from './Home';
 
 const routes = [
+  {
+    path: '/',
+    element: <Navigate to="/login" />,
+  },
   {
     path: '/login',
     element: <LoginForm />,
