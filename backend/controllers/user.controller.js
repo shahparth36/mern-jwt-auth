53f63e5e4870db9a409b18ae108104be2f30315a
shahparth36 <parthshah1936@gmail.com>
2021-08-16 16:51:24 +0530

    added service file for users

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,8 +1,8 @@
-const User = require("../models/index").user;
+const { _getUserById, _getAllUsers } = require("../services/user.service");
 
 const getUserById = async (req, res, next) => {
   try {
-    const user = await User.findById(req.params.userId);
+    const user = await _getUserById({ ...req.params });
     return res.status(200).json(user);
   } catch (error) {
     next(error);
@@ -11,7 +11,7 @@ const getUserById = async (req, res, next) => {
 
 const getAllUsers = async (req, res, next) => {
   try {
-    const allUsers = await User.find({});
+    const allUsers = await _getAllUsers();
     return res.status(200).json(allUsers);
   } catch (error) {
     next(error);
